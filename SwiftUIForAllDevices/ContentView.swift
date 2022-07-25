04f5a2ffb6f269e435bef2c2d5c299b726c038b4
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-24 21:37:41 -0700

    Initial Commit

---
+++ SwiftUIForAllDevices/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIForAllDevices
+//
+//  Created by Mitya Kim on 7/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIForAllDevices/SwiftUIForAllDevicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIForAllDevicesApp.swift
+//  SwiftUIForAllDevices
+//
+//  Created by Mitya Kim on 7/24/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIForAllDevicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
